9540de91a64b94523eb34d7d1d648cb3877d5dbf
DasherVX <116335934+DasherVX@users.noreply.github.com>
2022-10-21 17:14:08 +0800

    Update Coordinate.c
    midpoint of coordinates follow the formula (x1 + x2)/2 and (y1+y2)/2 respectively.

--- Coordinate.c
+++ Coordinate.c
@@ -10,8 +10,8 @@ int main() {
 	printf("Please Input the Second X and Y coordinates:");
 	scanf("%g, %g", &x2, &y2);
 	
-	res1 = (x1 + y1)/2;
-	res2 = (x2 + y2)/2;
+	res1 = (x1 + x2)/2;
+	res2 = (y1 + y2)/2;
 	
 	printf("%g,%g is the mid-point coordinate.", res1, res2);
 	
